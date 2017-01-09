da3bb0c94d17b1be0c491e72d15e4d125de25437
Roman Tumaykin <roman@tumaykin.com>
2017-01-09 01:25:51 -0800

    Added license, removed unused usings

--- src/SsisBuild.Builder/Decryptor.cs
+++ src/SsisBuild.Builder/Decryptor.cs
@@ -1,6 +1,21 @@
-﻿using System;
+﻿//-----------------------------------------------------------------------
+//Copyright 2017 Roman Tumaykin
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+//-----------------------------------------------------------------------
+
+using System;
 using System.IO;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Xml;
@@ -23,29 +38,9 @@ namespace SsisBuild
 
         private static XmlNodeList GetSensitiveNodes(XmlNode rootNode)
         {
-//            if (ContainsNamespace(rootNode))
-                return rootNode.SelectNodes("//*[@Sensitive=\"1\" or @SSIS:Sensitive=\"1\"]",
-                    GetNameSpaceManager(rootNode));
-//            return rootNode.SelectNodes("//*[@Sensitive=\"1\"]");
+            return rootNode.SelectNodes("//*[@Sensitive=\"1\" or @SSIS:Sensitive=\"1\"]",
+                GetNameSpaceManager(rootNode));
         }
-
-        //private static bool ContainsNamespace(XmlNode rootNode)
-        //{
-        //    if (rootNode.LocalName.ToUpperInvariant().Equals("Manifest".ToUpperInvariant(), StringComparison.Ordinal))
-        //    {
-        //        rootNode = rootNode.FirstChild;
-        //        if (rootNode == null)
-        //            return true;
-        //    }
-        //    var str = !(rootNode is XmlDocument)
-        //        ? rootNode.NamespaceURI
-        //        : (rootNode.ChildNodes.OfType<XmlNode>().FirstOrDefault(n => n.NodeType != XmlNodeType.XmlDeclaration)?.NamespaceURI ?? string.Empty);
-        //    return
-        //        str.ToUpperInvariant()
-        //            .Equals("www.microsoft.com/SqlServer/SSIS".ToUpperInvariant(), StringComparison.Ordinal) ||
-        //        str.ToUpperInvariant()
-        //            .Equals("www.microsoft.com/SqlServer/Dts".ToUpperInvariant(), StringComparison.Ordinal);
-        //}
 
         public static void DecryptXmlNode(XmlNode xmlNode, DTSProtectionLevel protectionLevel, string projectPassword)
         {
