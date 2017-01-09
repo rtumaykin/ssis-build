8397125392afd5e7231509cba48db74f02de4fa6
Roman Tumaykin <roman@tumaykin.com>
2017-01-09 02:11:08 -0800

    Added readme, changed usage

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# ssis-build
-Build runner for SQL Server Integration Services projects (Versions 2012-2016)
+# **SSISBuild**
+### A command line utility that builds an ispac file from an Visual Studio SSIS project (project deployment model only). 
+####**Syntax**
+
+	ssisbuild [Project File] [-<Switch Name> <Value>] [...[-<Switch Name> <Value>]] [-Parameter:<Name> <Value>] [...[-Parameter: <Name> <Value>]]
+
+####**Switches**
+Project File:
+: Full path to a SSIS project file (with dtproj extension). If a project file is not specified, ssisbuild searches current working directory for a file with dtproj extension and uses that file.
+
+-Configuration
+: Required. Name of project configuration to use.
+
+-OutputFolder:
+: Full path to a folder where the ispac file will be created. If ommitted, then the ispac file will be created in the bin/&lt;Configuration&gt; subfolder of the project folder.
+
+-ProtectionLevel:
+: Overrides current project protection level. Available values are `DontSaveSensitive`, `EncryptAllWithPassword`, `EncryptSensitiveWithPassword`.
+
+-Password:
+: Password to decrypt original project data if its current protection level is either `EncryptAllWithPassword` or `EncryptSensitiveWithPassword`,  in which case the value should be supplied, otherwise build will fail.
+
+-NewPassword:
+: Password to encrypt resulting project if its resulting protection level is either `EncryptAllWithPassword` or `EncryptSensitiveWithPassword`. If ommitted, the value of the &lt;Password&gt; switch is used for encryption, unless original protection level was `DontSaveSensitive`. In this case the value should be supplied, otherwise build will fail.
+
+-Parameter:
+: Project or Package parameter. Name is a standard full parameter name including the scope. For example `Project::Parameter1`. During the build, these values will replace existing values regardless of what they were originally.
+
+-SensitiveParameter:
+: Same as `-Parameter`, but during the build this value will be set to Sensitive, and therefore will be encrypted (or not saved if the target protection level is `DontSaveSensitive`.
+
+-ReleaseNotes:
+: Path to a release notes file. Supports simple or complex release notes format, as defined [here](http://fsharp.github.io/FAKE/apidocs/fake-releasenoteshelper.html).
+
+Example:
+: `ssisbuild example.dtproj -Configuration Release -Parameter:SampleParameter "some value"`
--- src/SsisBuild.Runner/Program.cs
+++ src/SsisBuild.Runner/Program.cs
@@ -44,41 +44,49 @@ namespace SsisBuild.Runner
 
         private static void Usage()
         {
-            Console.WriteLine("---------------------------------------------------------------");
-            Console.WriteLine("Usage:");
-            Console.WriteLine("");
-            Console.WriteLine("Syntax:                ssisbuild [Project File] [-<Switch Name> <Value>] [...[-<Switch Name> <Value>]] [-Parameter:<Name> <Value>] [...[-Parameter:<Name> <Value>]]");
-            Console.WriteLine("");
-            Console.WriteLine("Description:           Builds an SSIS project into an ispac file.");
-            Console.WriteLine("");
-            Console.WriteLine("Switches:");
-            Console.WriteLine("");
-            Console.WriteLine("  Project File:        Full path to a SSIS project file (with dtproj extension). If a project file is not specified, ssisbuild searches current working directory");
-            Console.WriteLine("                       for a file with dtproj extension and uses that file.");
-            Console.WriteLine("");
-            Console.WriteLine("  -Configuration:      Required. Name of project configuration to use.");
-            Console.WriteLine("");
-            Console.WriteLine("  -OutputFolder:       Full path to a folder where the ispac file will be created. If ommitted, then the ispac file will be created in the");
-            Console.WriteLine("                       bin/<Configuration> subfolder of the project folder.");
-            Console.WriteLine("");
-            Console.WriteLine("  -ProtectionLevel:    Overrides current project protection level. Available values are DontSaveSensitive, EncryptAllWithPassword, EncryptSensitiveWithPassword.");
-            Console.WriteLine("");
-            Console.WriteLine("  -Password:           Password to decrypt original project data if its current protection level is either EncryptAllWithPassword or EncryptSensitiveWithPassword, ");
-            Console.WriteLine("                       in which case the value should be supplied, otherwise build will fail.");
-            Console.WriteLine("");
-            Console.WriteLine("  -NewPassword:        Password to encrypt resulting project if its resulting protection level is either EncryptAllWithPassword or EncryptSensitiveWithPassword.");
-            Console.WriteLine("                       If ommitted, the value of the <Password> switch is used for encryption, unless original protection level was DontSaveSensitive,");
-            Console.WriteLine("                       in which case the value should be supplied, otherwise build will fail.");
-            Console.WriteLine("");
-            Console.WriteLine("  -Parameter:          Project or Package parameter. Name is a standard full parameter name including the scope. For example Project::Parameter1. During the build,");
-            Console.WriteLine("                       these values will replace existing values regardless of what these values were originally.");
-            Console.WriteLine("");
-            Console.WriteLine("  -SensitiveParameter: Project or Package parameter forced to be sensitive. Name is a standard full parameter name including the scope. For example Project::Parameter1. During the build,");
-            Console.WriteLine("                       these values will replace existing values regardless of what these values were originally.");
-            Console.WriteLine("  -ReleaseNotes:       Path to a release notes file. File can have simple or complex release notes format.");
-            Console.WriteLine("");
-            Console.WriteLine("Example:");
-            Console.WriteLine("     ssisbuild example.dtproj -Configuration Release -Parameter:SampleParameter \"some value\"");
+            var usage = new[]
+            {
+                "---------------------------------------------------------------",
+                "Usage:",
+                "",
+                "Syntax:                ssisbuild [Project File] [-<Switch Name> <Value>] [...[-<Switch Name> <Value>]] [-Parameter:<Name> <Value>] [...[-Parameter:<Name> <Value>]]",
+                "",
+                "Description:           Builds an SSIS project into an ispac file.",
+                "",
+                "Switches:",
+                "",
+                "  Project File:        Full path to a SSIS project file (with dtproj extension). If a project file is not specified, ssisbuild searches current working directory",
+                "                       for a file with dtproj extension and uses that file.",
+                "",
+                "  -Configuration:      Required. Name of project configuration to use.",
+                "",
+                "  -OutputFolder:       Full path to a folder where the ispac file will be created. If ommitted, then the ispac file will be created in the",
+                "                       bin/<Configuration> subfolder of the project folder.",
+                "",
+                "  -ProtectionLevel:    Overrides current project protection level. Available values are DontSaveSensitive, EncryptAllWithPassword, EncryptSensitiveWithPassword.",
+                "",
+                "  -Password:           Password to decrypt original project data if its current protection level is either EncryptAllWithPassword or EncryptSensitiveWithPassword, ",
+                "                       in which case the value should be supplied, otherwise build will fail.",
+                "",
+                "  -NewPassword:        Password to encrypt resulting project if its resulting protection level is either EncryptAllWithPassword or EncryptSensitiveWithPassword.",
+                "                       If ommitted, the value of the <Password> switch is used for encryption, unless original protection level was DontSaveSensitive,",
+                "                       in which case the value should be supplied, otherwise build will fail.",
+                "",
+                "  -Parameter:          Project or Package parameter. Name is a standard full parameter name including the scope. For example Project::Parameter1. During the build,",
+                "                       these values will replace existing values regardless of what these values were originally.",
+                "",
+                "  -SensitiveParameter: Project or Package parameter forced to be sensitive. Name is a standard full parameter name including the scope. For example Project::Parameter1. During the build,",
+                "                       these values will replace existing values regardless of what these values were originally.",
+                "  -ReleaseNotes:       Path to a release notes file. File can have simple or complex release notes format.",
+                "",
+                "Example:",
+                "     ssisbuild example.dtproj -Configuration Release -Parameter:SampleParameter \"some value\""
+            };
+
+            foreach (var usageLine in usage)
+            {
+                Console.WriteLine(usageLine);
+            }
         }
     }
 }
