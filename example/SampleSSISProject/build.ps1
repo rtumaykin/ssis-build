e2ac5402d07cbcbca904574a7335b00178a8cad5
Roman Tumaykin <roman@tumaykin.com>
2017-02-23 23:30:45 -0800

    Final version of the samle project with readme. Removed SQL 2016 sample

--- example/SampleSSISProject/build.ps1
+++ example/SampleSSISProject/build.ps1
@@ -1,9 +1,7 @@
 param (
-    [Parameter(Mandatory=$true)][string]$SSISInstanceName,
-    [Parameter(Mandatory=$true)][string]$SSISCatalog,
-    [Parameter(Mandatory=$true)][string]$SSISDeploymentFolder,
-    [Parameter(Mandatory=$true)][string]$SSISProjectName,
-    [Parameter(Mandatory=$true)][string]$Password,
+    [Parameter(Mandatory=$true)][string]$Configuration,
+    [string]$DeploymentProtectionLevel,
+    [string]$Password,
     [string]$NewPassword,
     [string]$SourceDBName,
     [string]$SourceDBServer,
@@ -14,25 +12,33 @@ Remove-Item -Path "build" -Recurse
 Remove-Item -Path "packages\SSISBuild" -Recurse
 
 # Make sure we have Nuget.exe
-.\ensureNuget.ps1
+$nugetFolder = Join-Path $env:LOCALAPPDATA "Nuget"
+
+$nugetExe = Join-Path $nugetFolder "Nuget.exe"
+
+if (-not (Test-Path $nugetFolder)) {
+    New-Item $nugetFolder -ItemType Directory
+}
+
+if (-not (Test-Path $nugetExe)) {
+    $ProgressPreference = "SilentlyContinue"
+    Invoke-WebRequest 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe' -OutFile $nugetExe
+}
+
+& $nugetExe update -self
 
 # Download SSISBuild
 $nugetExe = [System.IO.Path]::Combine($Env:LOCALAPPDATA, "Nuget", "Nuget.exe")
 & $nugetExe install SSISBuild -OutputDirectory "packages" -ExcludeVersion
 
+# Import SSISBuild Modules
+Import-Module .\packages\SSISBuild\tools\SsisBuild.Core.dll
 
+New-SsisDeploymentPackage '.\SampleSSISProject\SampleSSISProject.dtproj' -ProtectionLevel $DeploymentProtectionLevel -Configuration $Configuration -Password "$Password" -NewPassword "$NewPassword" -OutputFolder ".\build" -ReleaseNotes "$ReleaseNotesFilePath" -Parameters @{"Project::SourceDBServer" = $SourceDBServer; "Project::SourceDBName" = $SourceDBName}
 
-packages\SSISBuild\tools\ssisbuild 'SampleSSISProject\SampleSSISProject.dtproj' -Configuration Deployment -Password "$Password" -NewPassword "$NewPassword" -OutputFolder "build" -ReleaseNotes "$ReleaseNotesFilePath" "-Parameter:Project::SourceDBServer" "$SourceDBServer" "-Parameter:Project::SourceDBName" "$SourceDBName"
+# Copy module to the artifacts folder so we can use it during the deployment without having to redownload it
+Copy-Item .\packages\SSISBuild\tools\*.dll .\build
 
-# Copy SSISDeploy to the artifacts folder
-Copy-Item "packages\SSISBuild\tools\ssisdeploy.exe" "build\"
+# Copy deployment script to the artifacts folder
+Copy-Item .\deploy.ps1 .\build
 
-# Escape parenthesis in variables
-
-# Now create deploy.cmd file with all proper parameters
-$cmd = "@echo off
-pushd %~dp0
-ssisdeploy.exe 
-powershell -Command "".\deploy.ps1"" -SSISInstanceName `'$SSISInstanceName`' -SSISCatalog `'$SSISCatalog`' -SSISDeploymentFolder `'$SSISDeploymentFolder`' -SSISProjectName `'$SSISProjectName`'
-popd"
-Set-Content -Path build\deploy.cmd -Value $cmd
